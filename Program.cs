2570422d5b099a51f0e5f800b157f7c66168c8d7
riophs <62876900+riophs@users.noreply.github.com>
2025-04-13 17:32:53 +0100

    Acrescentar código para testar

--- Program.cs
+++ Program.cs
@@ -15,6 +15,12 @@ namespace Calculos
             console.WriteLine($"4 + 2 = {Aritmetica.Somar(4, 2)}");
             Console.WriteLine($"4 - 2 = {Aritmetica.Subtrair(4, 2)}");
 
+            double t1 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.CelsiusFahrenheit, 20);
+            double t2 = Conversoes.ConverterTemperatura(Conversoes.ConversaoTemperatura.FahrenheitCelsius, 68);
+
+            Console.WriteLine($"36º Celsius = {t1}º Fahrenheit");
+            Console.WriteLine($"100º Fahrenheit = {t2}º Celsius");
+            
             Console.ReadKey();
         }
     }
